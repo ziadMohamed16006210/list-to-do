3c90a2f6b5556e229c64e7b32f15ea4b6c12757f
ZyadAbdou05 <mziad908070@gmail.com>
2026-01-30 16:58:01 +0200

    full working version after adding the add button , delete

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,43 @@
-import AddTask from "./components/AddTask";
+"use client";
 
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <main>
-       <div>
-      <h1>To do list app</h1> 
+  const [todos, setTodos] = useState<string[]>([]);
+  const [todo, setTodo] = useState<string>("");
 
-      <AddTask/>
-       </div>
-      
-      </main>
-    
+  const updateToDoList = () => {
+    if (!todo.trim()) return;
+    setTodos([...todos, todo]);
+    setTodo("");
+  };
+
+  const deleteTodo = (index: number) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div>
+      <h1>To do list app</h1>
+
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+
+      <button onClick={updateToDoList}>Add Todo</button>
+
+      <ul>
+        {todos.map((value, index) => (
+          <li key={index}>
+            {value}
+            <button onClick={() => deleteTodo(index)}>
+              Delete Todo
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
